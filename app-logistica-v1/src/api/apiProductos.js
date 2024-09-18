fc7b4fbd87b757f271b416c20d8c6177360e4a7f
Daikus007 <moyafabian989@gmail.com>
2024-09-18 15:41:56 -0500

    Organizar carptesas y modificar apiProductos y ProductosComponent

--- app-logistica-v1/src/api/apiProductos.js
+++ app-logistica-v1/src/api/apiProductos.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiProductos = axios.create({
+const api = axios.create({
     baseURL: "http://localhost:8080/api",
     headers: {
         "Content-Type": "application/json",
--- app-logistica-v1/src/components/ProductosComponent.js
+++ app-logistica-v1/src/components/ProductosComponent.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { getProductos, createProducto, deleteProducto } from './apiProductos'; 
+import { getProductos, createProducto, deleteProducto } from './api/apiProductos'; 
 
 const ProductosComponent = () => {
     const [productos, setProductos] = useState([]);
