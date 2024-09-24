20fa6357a99cfabb737689db293a6bf59df4b64f
jotik <jotikgg77@gmail.com>
2024-09-24 12:11:25 -0500

    Unir back y front

--- app-logistica-v1/src/App.js
+++ app-logistica-v1/src/App.js
@@ -1,5 +1,5 @@
 // App.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './Components/Sidebar';
 import Dashboard from './Components/Dashboard';
@@ -11,22 +11,37 @@ import Miembros from './Components/Miembros';
 import './App.css';
 
 function App() {
+    const [users, setUsers] = useState([]); // Estado para los usuarios
+
+    useEffect(() => {
+        // Función para obtener los usuarios
+        const fetchUsers = async () => {
+            try {
+                const response = await fetch('http://localhost:8080/api/users');
+                const data = await response.json();
+                setUsers(data); // Guardar los usuarios en el estado
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+
+        fetchUsers(); // Llamar a la función al cargar el componente
+    }, []);
+
     return (
-
-            <div className="d-flex" id="wrapper">
-                <Sidebar />
-                <div id="page-content-wrapper">
-                    <Routes>
-                        <Route path="/dashboard" element={<Dashboard />} />
-                        <Route path="/productos" element={<Productos />} />
-                        <Route path="/categorias" element={<Categorias />} />
-                        <Route path="/almacen" element={<Almacen />} />
-                        <Route path="/pedidos" element={<Pedidos />} />
-                        <Route path="/miembros" element={<Miembros />} />
-                    </Routes>
-                </div>
+        <div className="d-flex" id="wrapper">
+            <Sidebar />
+            <div id="page-content-wrapper">
+                <Routes>
+                    <Route path="/dashboard" element={<Dashboard users={users} />} />
+                    <Route path="/productos" element={<Productos />} />
+                    <Route path="/categorias" element={<Categorias />} />
+                    <Route path="/almacen" element={<Almacen />} />
+                    <Route path="/pedidos" element={<Pedidos />} />
+                    <Route path="/miembros" element={<Miembros />} />
+                </Routes>
             </div>
-
+        </div>
     );
 }
 
